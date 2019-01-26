d9f00d040b218556c1ef16d3f4609be6beeda34c
MissGrass <1209784760@qq.com>
2019-01-26 19:11:09 +0800

    -

--- 1002. 写出这个数 (20).cpp
+++ 1002. 写出这个数 (20).cpp
@@ -1,6 +1,6 @@
 /*读入一个自然数n，计算其各位数字之和，用汉语拼音写出和的每一位数字。
 
-输入格式：每个测试输入包含1个测试用例，即给出自然数n的值。这里保证n小于10100。
+输入格式：每个测试输入包含1个测试用例，即给出自然数n的值。这里保证n小于10的100次方。
 
 输出格式：在一行内输出n的各位数字之和的每一位，拼音数字间有1 空格，但一行中最后一个拼音数字后没有空格。
 
@@ -9,16 +9,24 @@
 输出样例：
 yi san wu   */ 
 
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-	int n,i=0;
-	scanf("%d",&n);
-	
-	while(n!=0)
-	{
-		i+=(n%10);
-		n=n/10;
+	string n;
+	string py[10] = {"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
+	cin >> n;
+	int sum;
+	for(int i = 0; i < n.length(); i++){
+		sum += (n[i] - '0');
 	}
-	printf("%d",i);
+	string num = to_string(sum);
+	for(int i = 0; i < num.length(); i++){
+		cout << py[num[i] - '0'];
+		if(i != num.length()-1)
+			cout << " ";
+	}
 }
+
+
+
